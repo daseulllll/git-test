f99308eac676f3dc7159e95d6037eeda4bc06177
김다슬 <ektmf4785@naver.com>
2022-11-02 22:07:11 +0900

    vscode2

--- Hello/src/hellojava/Hello.java
+++ Hello/src/hellojava/Hello.java
@@ -7,6 +7,8 @@ public class Hello {
 		System.out.println("수정을 한 것이여`~");
 		System.out.println("지금은 9시 59분~");
 		System.out.println("윤찬쓰 라피협 듣는중");
+		System.out.println("무엇이 달라졌는가~~알아맞혀보시오~~");
+		System.out.println("vscode로 올리는건 괜찮은것같기도?");
 	}
 	
 }
