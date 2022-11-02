2e8af6b5981a05a5887f8fb2805d61e86de9d0d6
김다슬 <ektmf4785@naver.com>
2022-11-02 22:02:55 +0900

    vscode로 올리는중

--- Hello/src/hellojava/Hello.java
+++ Hello/src/hellojava/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("수정을 한 것이여`~");
-		System.out.println("난 브랜치에 올리고싶은디");
+		System.out.println("지금은 9시 59분~");
+		System.out.println("윤찬쓰 라피협 듣는중");
 	}
 	
 }
